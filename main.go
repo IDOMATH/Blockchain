accfedb065620c1fb6e9929c630528474b61fc1d
IDOMATH <btdothemath@gmail.com>
2024-02-06 14:52:25 -0600

    Adding peer to peer with grpc

--- main.go
+++ main.go
@@ -2,35 +2,29 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/idomath/Blockchain/node"
 	"github.com/idomath/Blockchain/proto"
 	"google.golang.org/grpc"
 	"log"
-	"net"
-	"time"
 )
 
 func main() {
-	node := node.NewNode()
+	makeNode(":3000", []string{})
+	makeNode(":4000", []string{":3000"})
 
-	opts := []grpc.ServerOption{}
-	grpcServer := grpc.NewServer(opts...)
-	ln, err := net.Listen("tcp", ":3000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	proto.RegisterNodeServer(grpcServer, node)
-	fmt.Println("node running on port: ", "3000")
+	select {}
+}
 
-	go func() {
-		for {
-			time.Sleep(2 * time.Second)
-			makeTransaction()
+func makeNode(listenAddr string, bootstrapNodes []string) *node.Node {
+	n := node.NewNode()
+	go n.Start(listenAddr)
+	if len(bootstrapNodes) > 0 {
+		if err := n.BootstrapNetwork(bootstrapNodes); err != nil {
+			log.Fatal(err)
 		}
-	}()
+	}
 
-	grpcServer.Serve(ln)
+	return n
 }
 
 func makeTransaction() {
@@ -51,7 +45,9 @@ func makeTransaction() {
 		log.Fatal(err)
 	}
 	version := &proto.Version{
-		Version: "0.1",
+		Version:    "0.1",
+		Height:     1,
+		ListenAddr: ":4000",
 	}
 
 	_, err = c.Handshake(context.TODO(), version)
--- node/node.go
+++ node/node.go
@@ -4,21 +4,78 @@ import (
 	"context"
 	"fmt"
 	"github.com/idomath/Blockchain/proto"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
+	"log"
 	"net"
+	"sync"
 )
 
 type Node struct {
-	version string
-	peers   map[net.Addr]*grpc.ClientConn
+	version    string
+	listenAddr string
+	logger     *zap.SugaredLogger
+	peerLock   sync.RWMutex
+	peers      map[proto.NodeClient]*proto.Version
 	proto.UnimplementedNodeServer
 }
 
 func NewNode() *Node {
+	loggerConfig := zap.NewDevelopmentConfig()
+	loggerConfig.EncoderConfig.TimeKey = ""
+	logger, _ := loggerConfig.Build()
 	return &Node{
 		version: "0.1",
+		peers:   make(map[proto.NodeClient]*proto.Version),
+		logger:  logger.Sugar(),
 	}
+}
+
+func (n *Node) BootstrapNetwork(addresses []string) error {
+	for _, addr := range addresses {
+		c, err := makeNodeClient(addr)
+		if err != nil {
+			return err
+		}
+		v, err := c.Handshake(context.Background(), n.getVersion())
+		if err != nil {
+			n.logger.Error("handshake error: ", err)
+			continue
+		}
+		n.addPeer(c, v)
+	}
+	return nil
+}
+
+func (n *Node) Start(listenAddr string) error {
+	n.listenAddr = listenAddr
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	ln, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	proto.RegisterNodeServer(grpcServer, n)
+
+	n.logger.Infow("Node started...", "port", listenAddr)
+
+	return grpcServer.Serve(ln)
+}
+
+func (n *Node) addPeer(c proto.NodeClient, v *proto.Version) {
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+
+	n.logger.Debugw("new peer connected", "addr", v.ListenAddr, "height", v.Height)
+
+	n.peers[c] = v
+}
+
+func (n *Node) deletePeer(c proto.NodeClient) {
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+	delete(n.peers, c)
 }
 
 func (n *Node) Handshake(ctx context.Context, v *proto.Version) (*proto.Version, error) {
@@ -27,8 +84,12 @@ func (n *Node) Handshake(ctx context.Context, v *proto.Version) (*proto.Version,
 		Height:  100,
 	}
 
-	p, _ := peer.FromContext(ctx)
-	fmt.Printf("received version: %+v from %s", v, p.Addr)
+	c, err := makeNodeClient(v.ListenAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	n.addPeer(c, v)
 
 	return ourVersion, nil
 }
@@ -37,4 +98,20 @@ func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*p
 	peer, _ := peer.FromContext(ctx)
 	fmt.Println("received tx from: ", peer)
 	return &proto.Ack{}, nil
+}
+
+func (n *Node) getVersion() *proto.Version {
+	return &proto.Version{
+		Version:    "0.1",
+		Height:     0,
+		ListenAddr: n.listenAddr,
+	}
+}
+
+func makeNodeClient(listenAddr string) (proto.NodeClient, error) {
+	c, err := grpc.Dial(listenAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return proto.NewNodeClient(c), nil
 }
